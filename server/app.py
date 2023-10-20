99d31594301a501002429562eba0893463ec170a
danwycliff <danwycliff6769@gmail.com>
2023-10-20 09:04:18 +0300

    codes

--- server/app.py
+++ server/app.py
@@ -19,54 +19,117 @@ db.init_app(app)
 def index():
     return "Index for Game/Review/User API"
 
-@app.route('/games')
-def games():
+@app.route('/reviews/<int:id>', methods=['GET','DELETE'])
+def review_by_id(id):
+    review = Review.query.filter(Review.id == id).first()
 
-    games = []
-    for game in Game.query.all():
-        game_dict = {
-            "title": game.title,
-            "genre": game.genre,
-            "platform": game.platform,
-            "price": game.price,
+    if request.method == 'GET':
+        review_dict = review.to_dict()
+
+        response = make_response(
+            review_dict,
+            200
+        )
+        return response
+    elif request.method == 'DELETE':
+        db.session.delete(review)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful":True,
+            "message":"Review deleted"
         }
-        games.append(game_dict)
+        response = make_response(
+            response_body,
+            200
+        )
+        return response
 
-    response = make_response(
-        games,
-        200
-    )
-
-    return response
-
-@app.route('/games/<int:id>')
-def game_by_id(id):
-    game = Game.query.filter(Game.id == id).first()
-    
-    game_dict = game.to_dict()
-
-    response = make_response(
-        game_dict,
-        200
-    )
-
-    return response
-
-@app.route('/reviews')
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
 
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
+    if request.method == 'GET':
+        reviews = []
+        for review in Review.query.all():
+            review_dict = review.to_dict()
+            reviews.append(review_dict)
 
-    response = make_response(
-        reviews,
-        200
-    )
+        response = make_response(
+            reviews,
+            200
+        )
 
-    return response
+        return response
+    
+    elif request.method == 'POST':
+        new_review = Review(
+            score=request.form.get("score"),
+            comment=request.form.get("comment"),
+            game_id=request.form.get("game_id"),
+            user_id=request.form.get("user_id"),
+        )
 
+        db.session.add(new_review)
+        db.session.commit()
+        
+        review_dict = new_review.to_dict()
+
+        response = make_response(
+            review_dict,
+            201
+        )
+
+        return response
+@app.route('/reviews/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def reviews_by_id(id):
+    review = Review.query.filter(Review.id == id).first()
+
+    if review == None:
+        response_body = {
+            "message":"This record does not exist in our database. please try again"
+        }
+        response = make_response(
+            response_body,
+            404
+        )
+        return response
+    else:
+        if request.method == 'GET':
+            review_dict = review.to_dict()
+            response = make_response(
+                review_dict,
+                200
+            )
+
+            return response
+        elif request.method == 'PATCH':
+            for attr in request.form:
+                setattr(review, attr, request.form.get(attr))
+            db.session.add(review)
+            db.session.commit()
+
+            review_dict = review.to_dict()
+
+            response = make_response(
+                review_dict,
+                200
+            )
+            return response
+        elif request.method == 'DELETE':
+            db.session.delete(review)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "Review deleted."    
+            }
+
+            response = make_response(
+                response_body,
+                200
+            )
+
+            return response
 @app.route('/users')
 def users():
 
@@ -81,6 +144,5 @@ def users():
     )
 
     return response
-
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
